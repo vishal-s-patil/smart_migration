6f7179b47675b901620f039a2f83f0e37f49f634
vishal-s-patil <vishalspolicepatil55@gmail.com>
2025-04-24 11:30:40 +0530

    Updated

--- smart_migration.py
+++ smart_migration.py
@@ -462,8 +462,7 @@ def delete_all_kafka_topics(*args, **kwargs):
             
         # Delete all topics
         admin_client.delete_topics(topics)
-        # commit the changes
-        admin_client.commit()
+        
         admin_client.close()
         return True, f"Successfully deleted {len(topics)} topics"
     except Exception as e:
