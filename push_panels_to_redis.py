73076794aec9d0bba5cc9cba97774762054d811e
vishal-s-patil <vishalspolicepatil55@gmail.com>
2025-04-25 13:11:47 +0530

    Updated

--- push_panels_to_redis.py
+++ push_panels_to_redis.py
@@ -257,7 +257,7 @@ if __name__ == "__main__":
         
         log_message("INFO", {"msg": f"starting to push {len(clients)} panels to redis"})
         
-        # push_panel_to_redis(clients, is_both)
+        push_panel_to_redis(clients, is_both)
 
         # for producer_method in producer_methods: # consumer_methods:
         #     producer_method = producer_method + "_queue"
